31955e48ff73f9b09ccf418c05dfd094cab708cc
Christian Heisig <cheisig@gmail.com>
2024-12-28 18:11:02 +0100

    Create __init__.py

---
+++ components/modbus_write/__init__.py
@@ -0,0 +1,9 @@
+# modbus_write/__init__.py
+
+"""
+Modbus write external component for ESPHome
+"""
+
+from esphome import Component
+
+# This could potentially be used to expose functions or classes
